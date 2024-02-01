eb5caa357f175f4457cf09de1a4f2e29b818ea22
di-saldana <dsl42@alu.ua.es>
2024-02-01 19:28:36 +0100

    Initial Commit

---
+++ NotasCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotasCoreData
+//
+//  Created by Dianelys Saldaña on 2/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
