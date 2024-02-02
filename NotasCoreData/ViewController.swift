aa571afa9e727a30e28836c6c9a064998692ca33
di-saldana <dsl42@alu.ua.es>
2024-02-02 15:49:35 +0100

    first commit

--- NotasCoreData/ViewController.swift
+++ NotasCoreData/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func create(_ sender: UIButton) {
+        dateLabel.text = ""
+        textView.text = ""
+        messageLabel.text = ""
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        guard let miDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let miContexto = miDelegate.persistentContainer.viewContext
+        
+        let nuevaNota = Nota(context: miContexto)
+        nuevaNota.fecha = Date()
+        nuevaNota.texto = textView.text
+        
+        do {
+            try miContexto.save()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .medium
+            dateFormatter.timeStyle = .medium
+            dateLabel.text = dateFormatter.string(from: nuevaNota.fecha!)
+            messageLabel.text = "Nota guardada correctamente"
+        } catch {
+            print("Error al guardar el contexto: \(error)")
+            messageLabel.text = "Error al guardar la nota"
+        }
+    }
 }
 
